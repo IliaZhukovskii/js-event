2a1c39cc52fc155024a2d2ede39c1a2d0fcb47cd
IliaZhukovskii <ilia.zhukovskii.99@mail.ru>
2022-04-07 10:50:22 +0300

    урок 11

--- script.js
+++ script.js
@@ -1,5 +1,32 @@
+"use strict";
 
+//Получение переменных 
+let square = document.getElementById('square');
+let text = document.getElementById('text');
+let button = document.getElementById('btn');
+let eBtn = document.getElementById('e_btn');
+let circle = document.getElementById('circle');
+let range = document.getElementById('range');
+let span = document.getElementById('range-span');
 
+//Функция изменения фона квадрата
+let background = function(){
+  square.style.background= text.value;
+  text.value = "";
+};
 
+//Клик изменения фона квадрата
+button.addEventListener('click', background);
 
+//Сделал кнопку невидимой
+eBtn.style.display = "none";
 
+//Функция изменения ширины и высоты круга
+let size = function(){
+  circle.style.width = range.value + "%";
+  circle.style.height = range.value + "%";
+  span.textContent = range.value;
+};
+
+//Изменение размеров круга через range
+range.addEventListener('input', size);
